181440a6971c6c2d54a991e3e4ebe0f0ab3bfe00
Heeyeon <heeyeon.lee0@gmail.com>
2025-05-21 13:16:10 -0700

    updated banner style and minor style update

--- src/components/ChatWidget.tsx
+++ src/components/ChatWidget.tsx
@@ -320,22 +320,7 @@ export function ChatWidget({
           </div>
           {maintenanceStatus.isInMaintenance && (
             <div className="maintenance-banner">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"></path>
-                <path d="M12 8v4"></path>
-                <path d="M12 16h.01"></path>
-              </svg>
-              <span>{maintenanceStatus.maintenanceMessage}</span>
+              We're performing maintenance. Chat will be back soon!
             </div>
           )}
           <div className="chat-input">
